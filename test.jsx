d4eb46f41fb9bf1e189f3d838013947dcf527cf4
NMS Mohamed <m.ezaizi@neusta.de>
2018-04-12 13:10:29 +0200

    jsx eingebunden

---
+++ test.jsx
@@ -0,0 +1,10 @@
+<form action="http://www.google.com/search">
+  <input type="text" name="q" />
+  <input type="submit" value="Go"
+    onclick="return confirm('Are you sure you want to search Google?')"
+  />
+ 
+</form>
+<button onClick = "googleFunction()"value = "test">
+
+</button>
